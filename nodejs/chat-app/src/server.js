e8250a3f0f02a0e14db4fb1d824f392bc014f8da
Roboking Master <robokingmaster@gmail.com>
2025-09-18 05:00:50 +0000

    Fix chat app

--- nodejs/chat-app/src/server.js
+++ nodejs/chat-app/src/server.js
@@ -11,7 +11,7 @@ const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 const publicDirectoyPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoyPath))
